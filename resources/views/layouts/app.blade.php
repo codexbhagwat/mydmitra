668105bc4968e281a84d3c9525354c61906f72e5
unknown <veeryodha323@gmail.comgit config --global user.email you@example.com>
2026-03-22 16:15:28 +0530

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 <title>@yield('title', 'Home') — Mydmitra</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="{{ asset('./css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body style="background:var(--white);">
 
